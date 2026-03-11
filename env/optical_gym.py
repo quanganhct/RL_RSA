a49b5be0a15f38e4ad0b48a8be91e9f967cf7633
quanganhct <quanganhct@gmail.com>
2026-03-11 16:18:06 -0400

    new

--- env/optical_gym.py
+++ env/optical_gym.py
@@ -1,8 +1,20 @@
 import gymnasium as gym
+from optical_network import Request, Path, Modulation, Lightpath, Network
+
+class PathSelectionAction:
+    def __init__(self, req:Request, path:Path, network:Network) -> None:
+        self.lightpath = Lightpath(req, path, network)
+        
+
+class ModulationSelectionAction:
+    def __init__(self, action1:PathSelectionAction, modulation:Modulation, starting_slot:int) -> None:
+        self.prev_action = action1
+        self.prev_action.lightpath.set_modulation(modulation)
+        self.starting_slot = starting_slot
 
 
 class RSAEnv(gym.Env):
-    def __init__(self):
+    def __init__(self, network:Network):
         super().__init__()
 
         ''' @routing_dict = {
@@ -12,5 +24,8 @@ class RSAEnv(gym.Env):
         }
         '''
         self.routing_dict = {}
+        self.network = network
 
-        
+    
+
+    
